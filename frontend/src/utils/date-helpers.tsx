52c6f2d9f45a5d15d5052e6ff7449d6e12765bee
Kenny Gozali <kennyftw2@gmail.com>
2025-08-21 22:48:30 +0200

    bugfix: was using minutes instead of months in the date

--- frontend/src/utils/date-helpers.tsx
+++ frontend/src/utils/date-helpers.tsx
@@ -1,5 +1,5 @@
 import { format, fromUnixTime } from 'date-fns';
 
 export const dbDateToRealDate = (dbDate: string) => {
-    return format(fromUnixTime(Number(dbDate)/1000), 'dd-mm-yyyy')
+    return format(fromUnixTime(Number(dbDate)/1000), 'dd-MM-yyyy')
   }
